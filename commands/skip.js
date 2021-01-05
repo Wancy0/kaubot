2f009a7ceea728a945631cb2ced07fa2fe05c52e
Wancy <63865530+Wancy0@users.noreply.github.com>
2021-01-05 15:42:53 +0000

    Add files via upload

---
+++ commands/skip.js
@@ -0,0 +1,13 @@
+const { canModifyQueue } = require("../util/EvobotUtil");
+
+module.exports = {
+  name: "skip",
+  aliases: ["s"],
+  description: "Skip the currently playing song",
+  execute(message) {
+    const queue = message.client.queue.get(message.guild.id);
+    if (!queue)
+      return message.reply("There is nothing playing that I could skip for you.").catch(console.error);
+    if (!canModifyQueue(message.member)) return;
+  }
+};
